2055ab318dae7c19f9ea4530d62c3d7cb8001964
Marcos Segantine <marcosegantine@hotmail.com>
2023-04-05 15:38:35 +0100

    Finalizado módulo de Controllers, realizado sobrecarga de rotas e visto diferentes tipos de ActionResult.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 
 namespace MyDemosMVC.Controllers
 {
+    //sobrecarga de rotas
+    [Route("")] //ao definir uma rota vazia há conflito com a rota de erro.
+    [Route("gestao-clientes")] //altera a rota base para dominio/gestao-clientes
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -12,18 +15,32 @@ namespace MyDemosMVC.Controllers
         {
             _logger = logger;
         }
-
-        public IActionResult Index()
+        [Route("")]
+        [Route("home")]//acrescenta o endereço na rota base: dominio/gestao-clientes/home
+        [Route("home/{id:guid}/{categoria:int?}")] //determina os tipos dos parametros necessario na rota
+        public IActionResult Index(Guid id, int categoria)
         {
             return View();
         }
 
+        [Route("privacidade")]//acrescenta o endereço na rota base: dominio/gestao-clientes/privacidade
+        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
-            return View();
+            //retornando um arquivo JSON
+            //return Json("{'Nome':'Marcos'}");
+
+            //retornando um conteudo
+            //return Content("Conteudo aleatorio");
+
+            //retornando um arquivo txt
+            var fileBytes = System.IO.File.ReadAllBytes(@"d:\arquivo.txt"); //selecionando arquivo no local fisico
+            var fileName = "ola.txt"; //dando nome do arquivo quando for baixado
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName); //retorna o arquivo e o nome que ele tera.
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("error")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
