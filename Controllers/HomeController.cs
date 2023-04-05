81651ec3c6801d8aded1aab14e297b9946da1526
Marcos Segantine <marcosegantine@hotmail.com>
2023-04-05 18:14:27 +0100

    Finalizado o modulo de Models, utilizando dataAnnotations e validando os dados.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,15 +18,38 @@ namespace MyDemosMVC.Controllers
         [Route("")]
         [Route("home")]//acrescenta o endereço na rota base: dominio/gestao-clientes/home
         [Route("home/{id:guid}/{categoria:int?}")] //determina os tipos dos parametros necessario na rota
+
+
         public IActionResult Index(Guid id, int categoria)
         {
-            return View();
+            var filme = new Filme
+            {
+                Titulo = "Te",
+                DataLancamento = DateTime.Now,
+                Genero = "Action",
+                Valor = 10000,
+                Avaliacao = 10
+            };
+            return RedirectToAction("Privacy", filme); //redireciona a var filme para a acttion privacy
+            //return View();
         }
 
         [Route("privacidade")]//acrescenta o endereço na rota base: dominio/gestao-clientes/privacidade
         [Route("politica-de-privacidade")]
-        public IActionResult Privacy()
+        public IActionResult Privacy(Filme filme)
         {
+            if (ModelState.IsValid) //verifica se o model é valido
+            {
+
+            }
+
+            foreach (var error in ModelState.Values.SelectMany(m => m.Errors))
+            
+            {
+                Console.WriteLine(error.ErrorMessage); //recupera os erros caso algum dado nao esteja de acordo com o especificado na model      
+            }
+
+            return View();
             //retornando um arquivo JSON
             //return Json("{'Nome':'Marcos'}");
 
@@ -34,9 +57,9 @@ namespace MyDemosMVC.Controllers
             //return Content("Conteudo aleatorio");
 
             //retornando um arquivo txt
-            var fileBytes = System.IO.File.ReadAllBytes(@"d:\arquivo.txt"); //selecionando arquivo no local fisico
+            /*var fileBytes = System.IO.File.ReadAllBytes(@"d:\arquivo.txt"); //selecionando arquivo no local fisico
             var fileName = "ola.txt"; //dando nome do arquivo quando for baixado
-            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName); //retorna o arquivo e o nome que ele tera.
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);*/ //retorna o arquivo e o nome que ele tera.
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/Filme.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyDemosMVC.Models
+{
+    public class Filme
+    {
+        [Key]
+        public int Id { get; set; } //caso va ser salva no DB
+
+        [Required(ErrorMessage ="O campo Título é obrigatório!")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage ="O título precisar ter entre 3 e 20 caracteres")]
+        public string? Titulo { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage ="Formato de data não está correto!")]
+        [Display(Name ="Data de Lançamento")] //altera como sera exibido
+        public DateTime DataLancamento { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$")] //inicia com maiuscula, depois minuscula e aceita acentuação
+        public string? Genero { get; set; }
+
+        [Range(1,1000)] //entre 1 e 1000
+        [Required(ErrorMessage ="Campo obrigatório")]
+        [Column(TypeName ="decimal(18,2)")] //tamanho maximo a ser criado no banco de dados
+        public decimal Valor { get; set; }
+
+        [RegularExpression(@"^[0-5]*$", ErrorMessage ="Somente números")]
+        [Display(Name ="Avaliação")]
+        public int Avaliacao { get; set; }
+    }
+}
